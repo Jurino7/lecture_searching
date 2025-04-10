1e642a641587ec700c7a905a0fd3fbc71dcf8718
Juraj Londak <londakjurino7@gmail.com>
2025-04-10 09:34:13 +0200

    final

--- searching.py
+++ searching.py
@@ -1,7 +1,6 @@
 import os
 import json
 
-
 # get current working directory path
 cwd_path = os.getcwd()
 
@@ -22,9 +21,60 @@ def read_data(file_name, field):
         return data[field]
 
 
-def main():
-    print(read_data("sequential.json", "unordered_numbers"))
+# best O(n) worst O(n)
+def linear_search(seq: list[int], to_find: int) -> dict[str: list[int], str: int]:
+    output = {"positions" : [],
+              "count" : 0}
 
+    for pos, number in enumerate(seq):
+        if number == to_find:
+            output["positions"].append(pos)
+            output["count"] += 1
+
+    return output
+
+
+def pattern_search(seq: str, pattern: str) -> set[int]:
+    output = set()
+
+    for i in range(len(seq)):
+        if i >= len(seq) - len(pattern):
+            break
+
+        if seq[i:i + len(pattern)] == pattern:
+            output.add(i)
+
+    return output
+
+
+def binary_search(seq: list[int], to_find: int) -> int | None:
+    left = 0
+    right = len(seq)
+
+    while left < right:
+
+        middle = (left + right) // 2
+
+        if seq[middle] == to_find and seq[middle - 1] == to_find:
+            right -= 1
+        elif seq[middle] == to_find:
+            return middle
+        elif to_find < seq[middle]:
+            right = middle - 1
+        else:
+            left = middle + 1
+
+    return
+
+
+def main():
+    print(read_data("sequential.json", "ordered_numbers"))
+    # print(linear_search(read_data("sequential.json", "unordered_numbers"), 9))
+    # print(pattern_search(read_data("sequential.json", "dna_sequence"), "ATA"))
+
+    number = 14
+    print("moja funkcia:\t\t", binary_search(read_data("sequential.json", "ordered_numbers"), number))
+    print("index of funkcia:\t", read_data("sequential.json", "ordered_numbers").index(number))
 
 if __name__ == '__main__':
     main()
